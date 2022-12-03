2fac9316312278abcc6d8e782eb437c92e7bc31b
Luke <luke.freeman4@gmail.com>
2022-12-03 15:24:01 -0600

    finished webscraper

--- scrapeWiki.py
+++ scrapeWiki.py
@@ -1,14 +1,21 @@
 import os
 from  selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.chrome.options import Options
+import random
 
 #"/home/luke/Documents/Selenium/chromedriver.exe
 os.environ['PATH'] += r"/home/luke/Documents/Selenium"
 driver = webdriver.Chrome()
-driver.get("https://www.wikipedia.org")
-driver.implicitly_wait(3)
-my_element = driver.find_element(By.ID, 'js-link-box-en')
-my_element.click()
-driver.implicitly_wait(10)
 
+
+def scrape_Wiki_Articles():
+    url = "https://en.wikipedia.org/wiki/Web_scraping"
+    response = driver.get(url)
+    driver.implicitly_wait(3)
+    my_element = driver.find_element(By.ID, 'firstHeading').text
+
+    all_links = driver.find_elements(By.TAG_NAME, 'a')
+    random_link = random.choice(all_links)
+    link_to_scrape = random_link.get_attribute('href')
+    print(link_to_scrape)
